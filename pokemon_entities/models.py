9b833312201c991f01bb6ecf11814218868f6082
e13q <110967581+e13q@users.noreply.github.com>
2024-07-12 22:11:38 +0300

    create Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,11 @@
 from django.db import models  # noqa F401
 
-# your models here
+class Pokemon(models.Model):
+    pokemon_id = models.AutoField(auto_created=True, primary_key=True)
+    title = models.CharField(max_length=200, unique=True)
+    #title_en = models.CharField(max_length=200, unique=True)
+    #title_jp = models.CharField(max_length=200, unique=True)
+    #description = models.TextField()
+    #img_url = models.TextField()
+    #entities = models.ForeignKey()
+    #next_evolution = models.ForeignKey()
