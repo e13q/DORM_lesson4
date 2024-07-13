38e8b0beaef6737b03bda827372de318fa9eb3b7
e13q <110967581+e13q@users.noreply.github.com>
2024-07-13 14:08:18 +0300

    add PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,5 +5,11 @@ class Pokemon(models.Model):
     pokemon_id = models.AutoField(auto_created=True, primary_key=True)
     title = models.CharField(max_length=200, unique=True)
     image = models.ImageField(upload_to='pokemons', null=True, blank=True)
+
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
