f0865bfdbc416c5e035d5b5797f8edba0d061aa2
e13q <110967581+e13q@users.noreply.github.com>
2024-07-13 14:27:38 +0300

    add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -21,3 +21,8 @@ class PokemonEntity(models.Model):
     lon = models.FloatField()
     appeared_at = models.DateTimeField(null=True, blank=True)
     disappeared_at = models.DateTimeField(null=True, blank=True)
+    level = models.IntegerField(null=True, blank=True)
+    health = models.IntegerField(null=True, blank=True)
+    strength = models.IntegerField(null=True, blank=True)
+    defence = models.IntegerField(null=True, blank=True)
+    stamina = models.IntegerField(null=True, blank=True)
