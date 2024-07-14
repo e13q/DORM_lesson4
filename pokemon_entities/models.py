c8b0a14dd8db727226e0ebd050facacd43dac114
e13q <110967581+e13q@users.noreply.github.com>
2024-07-14 12:26:04 +0300

    delete pokemon_id field for default id using

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,6 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    pokemon_id = models.AutoField(
-        auto_created=True,
-        primary_key=True,
-        verbose_name='id покемона'
-    )
     title_ru = models.CharField(
         max_length=200,
         unique=True,
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -49,7 +49,7 @@ def show_all_pokemons(request):
     pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
         pokemons_on_page.append({
-            'pokemon_id': pokemon.pokemon_id,
+            'pokemon_id': pokemon.id,
             'img_url': get_full_url(request, pokemon.image.url),
             'title_ru': pokemon.title_ru,
         })
@@ -62,7 +62,7 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     local_time = localtime()
-    requested_pokemon = Pokemon.objects.get(pokemon_id=pokemon_id)
+    requested_pokemon = Pokemon.objects.get(id=pokemon_id)
 
     pokemons_on_map = requested_pokemon.pokemon_entities.filter(
         disappeared_at__gt=local_time,
@@ -78,7 +78,7 @@ def show_pokemon(request, pokemon_id):
             get_full_url(request, pokemon.pokemon.image.url)
         )
     pokemon_to_view = {
-        'pokemon_id': requested_pokemon.pokemon_id,
+        'pokemon_id': requested_pokemon.id,
         'img_url': get_full_url(request, requested_pokemon.image.url),
         'description': requested_pokemon.description,
         'title_ru': requested_pokemon.title_ru,
@@ -87,7 +87,7 @@ def show_pokemon(request, pokemon_id):
     }
     if requested_pokemon.previous_evolution:
         previous_evolution = {
-            'pokemon_id': requested_pokemon.previous_evolution.pokemon_id,
+            'pokemon_id': requested_pokemon.previous_evolution.id,
             'img_url': get_full_url(
                 request,
                 requested_pokemon.previous_evolution.image.url
@@ -99,7 +99,7 @@ def show_pokemon(request, pokemon_id):
     if next_evolutions:
         next_evolution = next_evolutions[0]
         next_evolution = {
-            'pokemon_id': next_evolution.pokemon_id,
+            'pokemon_id': next_evolution.id,
             'img_url': get_full_url(request, next_evolution.image.url),
             'title_ru': next_evolution.title_ru
         }
